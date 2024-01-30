90d48653cd0228056ef690df2bbbd271dfa98c2a
Raineriojr <raijunior20@gmail.com>
2024-01-30 14:34:14 -0300

    feat: create initial page application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-personal-ws';
+  title: string = 'angular-personal-ws';
+
+  skillsList: Array<{skill: string, years: string}> = [
+    { skill: 'Mobile Developer', years: '3 Years'},
+    { skill: 'Frontend Developer', years: '3 Years'},
+    { skill: 'Backend Developer', years: '1.5 Years'},
+  ]
+
+  setItemStyle(index: number): string {
+    return index % 2 === 0 ? 'pill-list' : ''
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { HttpClientModule } from '@angular/common/http';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { ImageComponent } from './components/image/image.component';
+import { PillComponent } from './components/pill/pill.component';
+import { WorkCardComponent } from './components/work-card/work-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    CardComponent,
+    BigCardComponent,
+    ImageComponent,
+    PillComponent,
+    WorkCardComponent
   ],
   imports: [
     BrowserModule,
