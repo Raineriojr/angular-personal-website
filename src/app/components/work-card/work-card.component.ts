a0aa6d0978b4fc52a8e6568dc5e512f549503809
Raineriojr <raijunior20@gmail.com>
2024-02-07 08:22:19 -0300

    feat: config work card parms

--- src/app/components/work-card/work-card.component.ts
+++ src/app/components/work-card/work-card.component.ts
@@ -6,6 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./work-card.component.css']
 })
 export class WorkCardComponent {
+  @Input({ required: true }) id: string = '0';
   @Input({ required: true }) title: string = '';
   @Input({ required: true }) subtitle: string = ''
+  @Input({ required: true }) img: string = ''
 }
