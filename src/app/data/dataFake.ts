7728a5cd73e74f7a87f3f69ba1a9e2eea9244523
Raineriojr <raijunior20@gmail.com>
2024-02-07 08:27:01 -0300

    feat: add fake data

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    id: "1",
+    photo: "https://elements-cover-images-0.imgix.net/c9394a73-ad03-4c47-b62b-d1687f1e15cf?auto=compress%2Cformat&w=900&fit=max&s=aebe503f67f4d13ec2516c575dca74da",
+    title: "Projeto de X",
+    description: "Aplicativo mobile para gestão de time e negócios"
+  },
+  {
+    id: "2",
+    photo: "https://miro.medium.com/v2/resize:fit:916/1*z2BaY_MTgmcjgLfNyGQJ4g.png",
+    title: "Aplicativo de comida",
+    description: "Design ui de app mobile gerado por IA"
+  },
+  {
+    id: "3",
+    photo: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs3/140933666/original/d537503e602fbb0afada5546bd3fc2804cfc7480/design-professional-mobile-ui-with-prototype.png",
+    title: "Aplicativo de comida",
+    description: "Concorrente do aplicativo anterior"
+  }
+]
