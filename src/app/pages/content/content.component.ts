1abbfb40c5bbafe6838001760ac9aaf12a029b17
Raineriojr <raijunior20@gmail.com>
2024-02-07 08:21:53 -0300

    feat: add routing

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  @Input() photoCover: string = '';
+  @Input() content_title: string = '';
+  @Input() content_description: string = '';
+  @Input() private id: string | null = "0";
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(values => {
+      this.id = values.get('id');
+
+      this.setValuesInComponent(this.id)
+    })
+  }
+
+  setValuesInComponent(id: string | null): void {
+    const [item] = dataFake.filter((item, index) => { return item.id === this.id })
+
+    this.content_title = item.title;
+    this.content_description = item.description
+    this.photoCover = item.photo
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
+import { dataFake } from 'src/app/data/dataFake';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
   year: string = new Date().getFullYear().toString();
+
+  dataFake: typeof dataFake = dataFake;
 
   skillsList: Array<{skill: string, years: string}> = [
     { skill: 'Mobile Developer', years: '3 Years'},
